1f65d77c829da920584cc8820620fb8d16bda5a7
KIOLuo24 <chinese.developer@decentinvestmentlimited.co.uk>
2026-08-22 21:50:41 +0800

    Create coi-serviceworker.js

---
+++ coi-serviceworker.js
@@ -0,0 +1,33 @@
+/*! coi-serviceworker v0.1.7 - Guido Zouros */
+if (typeof window === 'undefined') {
+    self.addEventListener('install', () => self.skipWaiting());
+    self.addEventListener('activate', (event) => event.waitUntil(self.clients.claim()));
+    self.addEventListener('fetch', (event) => {
+        if (event.request.cache === 'only-if-cached' && event.request.mode !== 'same-origin') return;
+        event.respondWith(
+            fetch(event.request).then((response) => {
+                if (response.status === 0) return response;
+                const newHeaders = new Headers(response.headers);
+                newHeaders.set('Cross-Origin-Embedder-Policy', 'require-corp');
+                newHeaders.set('Cross-Origin-Opener-Policy', 'same-origin');
+                return new Response(response.body, {
+                    status: response.status,
+                    statusText: response.statusText,
+                    headers: newHeaders,
+                });
+            })
+        );
+    });
+} else {
+    const script = document.currentScript;
+    if (script && !window.crossOriginIsolated) {
+        navigator.serviceWorker.register(script.src).then((reg) => {
+            reg.addEventListener('updatefound', () => {
+                window.location.reload();
+            });
+            if (reg.active && !navigator.serviceWorker.controller) {
+                window.location.reload();
+            }
+        });
+    }
+}
